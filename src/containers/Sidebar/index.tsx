1483a45949d7b82d1b0e15fca3276a490feb1e31
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-08 08:31:08 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/diegocavalcanti-dev.png" />
+    <Titulo fontSize={20}>Diego Cavalcanti</Titulo>
+  </aside>
+)
+
+export default Sidebar
