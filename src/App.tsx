66ee6aafba4018dea65770fd4d71b2277e058a75
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-08 15:01:34 -0300

    acionando temas dark e light

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [estadoUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estadoUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estadoUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
